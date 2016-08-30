7f14e5c8505496ffedd14b28fc4d1011ef6e0850
Sid Ngeth <sngeth@gmail.com>
2016-08-30 14:15:58 -0400

    Convert rgb values to cmyk

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'pry'
+gem 'pry-byebug'
 gem 'rake'
--- src/color_kata/color.rb
+++ src/color_kata/color.rb
@@ -1,2 +1,14 @@
 class Color
+  attr_accessor :rgb_array
+
+  def initialize(args)
+    @rgb_array = Array.new
+    rgb_array << args[:r]
+    rgb_array << args[:g]
+    rgb_array << args[:b]
+  end
+
+  def cmyk_values
+    Converter.new(rgb: @rgb_array).to_cmyk
+  end
 end
