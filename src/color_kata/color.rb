921ded098828d430dd1d618e7dba6600f5988f0b
Sid Ngeth <sngeth@gmail.com>
2016-08-30 14:21:21 -0400

    Convert cmyk values to rgb values

--- src/color_kata/color.rb
+++ src/color_kata/color.rb
@@ -1,14 +1,26 @@
 class Color
-  attr_accessor :rgb_array
+  attr_accessor :color_array
 
   def initialize(args)
-    @rgb_array = Array.new
-    rgb_array << args[:r]
-    rgb_array << args[:g]
-    rgb_array << args[:b]
+    @color_array = Array.new
+
+    if args.keys == [:r, :g, :b]
+      color_array << args[:r]
+      color_array << args[:g]
+      color_array << args[:b]
+    else
+      color_array << args[:c]
+      color_array << args[:m]
+      color_array << args[:y]
+      color_array << args[:k]
+    end
   end
 
   def cmyk_values
-    Converter.new(rgb: @rgb_array).to_cmyk
+    Converter.new(rgb: @color_array).to_cmyk
+  end
+
+  def rgb_values
+    Converter.new(cmyk: @color_array).to_rgb
   end
 end
